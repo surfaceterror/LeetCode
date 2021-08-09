1bf0a62d715eb55cc8b63f73809df1e1c6ebed38
surfaceterror <46932960+surfaceterror@users.noreply.github.com>
2021-08-09 20:50:41 +0100

    Update Reverse Integer.js

--- Reverse Integer.js
+++ Reverse Integer.js
@@ -1,28 +1,28 @@
 const doThings = (x) => {
-  // convert to string
+  	// convert to string
 	let sx = x.toString();
   
 	let len = sx.length;
 	let output = [];
   
-  // creates an array of the numbers
+  	// creates an array of the numbers
 	for (let i = 0; i < len; i++) {
 		output.push(sx[i]);
 	}
   
-  // Reversed the array
+  	// Reversed the array
 	output.reverse();
   
-  // Removes the '-' from the end of the array and puts it at the beginning
+  	// Removes the '-' from the end of the array and puts it at the beginning
 	if (output.includes('-')) {
 		output.pop();
 		output.unshift('-');
 	}
   
-  // Joins the array
+  	// Joins the array
 	let join = output.join();
   
-  // Removes the ',' from the join
+  	// Removes the ',' from the join
 	let replace = join.replace(/,/g, '');
   
 	// Checks if replace is within range
